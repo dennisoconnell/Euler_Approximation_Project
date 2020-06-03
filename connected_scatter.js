e5afc8a773c1d89b8705c609255426f75aed6c5e
Dennis O'Connell <doconnell@scu.edu>
2020-06-03 09:35:13 -0700

    changed scale of connected plot

--- connected_scatter.js
+++ connected_scatter.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 100, bottom: 30, left: 30},
     width = 2*600 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 800 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
