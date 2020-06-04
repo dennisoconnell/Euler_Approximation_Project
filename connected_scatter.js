88c5489a7578ca8b4784d18657ea7a05b1f30b40
Dennis O'Connell <doconnell@scu.edu>
2020-06-04 10:50:17 -0700

    added more work to problem 2

--- connected_scatter.js
+++ connected_scatter.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 100, bottom: 30, left: 30},
-    width = 2*600 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom;
+    width = 600 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
